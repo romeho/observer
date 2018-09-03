a00471618581ae8329fc342f901411bc74499dd3
Romero Silva <33010752+romeho@users.noreply.github.com>
2018-09-03 10:16:37 -0300

    romeho

--- Launch.java
+++ Launch.java
@@ -1,14 +1,14 @@
 package romero.pg.avancada;
 
 public class Launch {
-	
+
 	public static void main(String[] args) {
-		
+
 		Estante estante = new Estante();
-		
+
 		EstatisticasEstante estatisticasEstante = new EstatisticasEstante(estante);
-		
-		estante.setDados(1, "Cem anos de solidão");
+
+		estante.setDados(1, "Cem anos de solidao");
 		estatisticasEstante.livro();
 		estante.addObserver(estatisticasEstante);
 	}
